70d2a5fe81b99910ce7e85a04be61afc7e6853ce
pricheal <patrickricheal@gmail.com>
2018-11-12 15:11:28 -0500

    Remove unused data

--- index.php
+++ index.php
@@ -6,6 +6,6 @@ require_once 'db.php';
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader);
 
-echo $twig->render('index.html', array('name' => 'Fabien'));
+echo $twig->render('index.html', array());
 
 ?>
