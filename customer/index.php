958ef4f40967545fe97ffe1727d191d76195617c
pricheal <patrickricheal@gmail.com>
2018-11-14 21:57:44 -0500

    Setup customer page to grab from customer view, also display address

--- customer/index.php
+++ customer/index.php
@@ -4,8 +4,10 @@ require_once '../global.php';
 require_once '../db.php';
 
 $customer_id = htmlspecialchars($_GET['id']);
+$source = htmlspecialchars($_GET['source']);
 
-$sql = "SELECT * FROM customer WHERE customer_id = ".$customer_id;
+$sql = "SELECT * FROM customer_view WHERE customer_id = ".$customer_id." AND source = '".$source."'";
+
 $result = mysqli_query($conn, $sql);
 $customer = mysqli_fetch_assoc($result);
 
