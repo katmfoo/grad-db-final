84cfc7182e883f053ab26b51e3c98f0ff27e6b85
pricheal <patrickricheal@gmail.com>
2018-11-14 21:22:47 -0500

    Add address to add customer

--- scripts/add_customer.php
+++ scripts/add_customer.php
@@ -5,14 +5,29 @@ require_once '../db.php';
 
 $first_name = htmlspecialchars($_POST['first_name']);
 $last_name = htmlspecialchars($_POST['last_name']);
+$address = htmlspecialchars($_POST['address']);
+$city = htmlspecialchars($_POST['city']);
+$state = htmlspecialchars($_POST['state']);
+$zip = htmlspecialchars($_POST['zip']);
+$country = htmlspecialchars($_POST['country']);
 
-$sql = "INSERT INTO customer (first_name, last_name) VALUES ('".$first_name."', '".$last_name."')";
+$sql = "INSERT INTO address (address, city, state, zip, country) VALUES ('".$address."', '".$city."', '".$state."', '".$zip."', '".$country."')";
+
+if (mysqli_query($conn, $sql)) {
+    $address_id = mysqli_insert_id($conn);
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    return;
+}
+
+$sql = "INSERT INTO customer (first_name, last_name, address_id) VALUES ('".$first_name."', '".$last_name."', '".$address_id."')";
 
 if (mysqli_query($conn, $sql)) {
     $_SESSION['flash'] = "Customer created successfully";
     header('Location: '.$_ROOT.'/customers/');
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    return;
 }
 
 ?>
