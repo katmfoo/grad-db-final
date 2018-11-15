1deeb37beadd1ff645513ffd9fa35112983419d9
pricheal <patrickricheal@gmail.com>
2018-11-15 16:58:35 -0500

    Set customers to use seller_id instead of source

--- customer/index.php
+++ customer/index.php
@@ -4,9 +4,9 @@ require_once '../global.php';
 require_once '../db.php';
 
 $customer_id = htmlspecialchars($_GET['id']);
-$source = htmlspecialchars($_GET['source']);
+$seller_id = htmlspecialchars($_GET['seller_id']);
 
-$sql = "SELECT * FROM customer_view WHERE customer_id = ".$customer_id." AND source = '".$source."'";
+$sql = "SELECT * FROM customer_view WHERE customer_id = ".$customer_id." AND seller_id = '".$seller_id."'";
 
 $result = mysqli_query($conn, $sql);
 $customer = mysqli_fetch_assoc($result);
