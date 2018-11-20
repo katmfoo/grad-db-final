b60dc582ee0ce405a1b52766a12e9632f7a5eb90
pricheal <patrickricheal@gmail.com>
2018-11-19 19:51:51 -0500

    Error check on order creation

--- scripts/create_order.php
+++ scripts/create_order.php
@@ -21,11 +21,15 @@ $result = mysqli_query($conn, $sql);
 $shipping_address_id = mysqli_fetch_assoc($result)['shipping_address_id'];
 
 $sql = "SELECT create_order('".$customer_id."', '".$customer_seller_id."', '".$shipping_address_id."') as order_id";
-
 $result = mysqli_query($conn, $sql);
-$order_id = mysqli_fetch_assoc($result)['order_id'];
 
-$_SESSION['flash'] = "Order created successfully";
-header('Location: '.$_ROOT.'/orders/?id='.$order_id);
+if ($result) {
+    $order_id = mysqli_fetch_assoc($result)['order_id'];
+    $_SESSION['flash'] = "Order created successfully";
+    header('Location: '.$_ROOT.'/orders/?id='.$order_id);
+} else {
+    $_SESSION['flash_error'] = "Product does not have enough stock";
+    header('Location: '.$_ROOT.'/cart/');
+}
 
 ?>
