1c68845e5c84eb43892912d0109877dfe811cb66
pricheal <patrickricheal@gmail.com>
2018-11-19 20:22:56 -0500

    Log out on customer delete if logged in

--- scripts/delete_customer.php
+++ scripts/delete_customer.php
@@ -7,6 +7,12 @@ $customer_id = htmlspecialchars($_GET['id']);
 $sql = "UPDATE customer SET is_deleted = 1 WHERE customer_id = ".$customer_id;
 
 if (mysqli_query($conn, $sql)) {
+
+    if (isset($_SESSION['current_customer_id']) && isset($_SESSION['current_customer_seller_id']) && $_SESSION['current_customer_seller_id'] == 1 && $_SESSION['current_customer_id'] == $customer_id) {
+        unset($_SESSION['current_customer_id']);
+        unset($_SESSION['current_customer_seller_id']);
+    }
+
     $_SESSION['flash'] = "Customer deleted successfully";
     header('Location: '.$_ROOT.'/customers/');
 } else {
