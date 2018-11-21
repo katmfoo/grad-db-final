a643c59a0223fab9beecef2eda171b19298c6328
pricheal <patrickricheal@gmail.com>
2018-11-20 19:05:10 -0500

    Add inventory and restocking order pages

---
+++ inventory/index.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once '../global.php';
+
+if (isset($_GET['page'])) {
+    $page = htmlspecialchars($_GET['page']);
+} else {
+    $page = 1;
+}
+
+$offset = $_ITEMS_PER_PAGE * ($page - 1);
+
+if (isset($_GET['search'])) {
+    $search = htmlspecialchars($_GET['search']);
+}
+
+$sql = "SELECT * FROM product WHERE is_deleted = 0 ";
+if (isset($search) && $search) { $sql = $sql."AND name LIKE '%".$search."%' "; }
+$sql = $sql."ORDER BY name ASC LIMIT ".$_ITEMS_PER_PAGE." OFFSET ".$offset;
+$result = mysqli_query($conn, $sql);
+$products = array();
+
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        array_push($products, $row);
+    }
+}
+
+$vars = array('products' => $products, 'page' => $page, 'page_size' => $_ITEMS_PER_PAGE);
+if (isset($search) && $search) { $vars['search'] = $search; }
+
+echo $twig->render('inventory.html', $vars);
+
+?>
---
+++ restocking-order/index.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once '../global.php';
+
+if (isset($_GET['page'])) {
+    $page = htmlspecialchars($_GET['page']);
+} else {
+    $page = 1;
+}
+
+$offset = $_ITEMS_PER_PAGE * ($page - 1);
+
+$sql = "SELECT * FROM product WHERE is_deleted = 0 AND current_stock < minimum_stock ORDER BY minimum_stock - current_stock DESC LIMIT ".$_ITEMS_PER_PAGE." OFFSET ".$offset;
+$result = mysqli_query($conn, $sql);
+$products = array();
+
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        array_push($products, $row);
+    }
+}
+
+echo $twig->render('restocking_order.html', array('products' => $products, 'page' => $page, 'page_size' => $_ITEMS_PER_PAGE));
+
+?>
