99b3b81ba51b1546c73dac2d397d53a5bda63963
pricheal <patrickricheal@gmail.com>
2018-12-03 21:34:21 -0500

    Add products most wished for by category

--- products-most-wished/index.php
+++ products-most-wished/index.php
@@ -8,33 +8,39 @@ if (isset($_GET['page'])) {
     $page = 1;
 }
 
+$_ITEMS_PER_PAGE = 4;
+
 $offset = $_ITEMS_PER_PAGE * ($page - 1);
 
 if (isset($_GET['search'])) {
     $search = htmlspecialchars($_GET['search']);
 }
 
-/* Selects all products that either have not been purchased at all (appears on the top of the list) or
-   products that haven't been purchased in over a month, with products having not been purchased for longer
-   appearing first */
+/* Selects the top 3 products that have been wished for the most from each product category,
+   does not include categories that no one has wished for a product with that category */
 
-$sql = "SELECT *, (
-	        SELECT creation_datetime FROM order_product WHERE product_id = product.product_id AND product_seller_id = 1 ORDER BY creation_datetime DESC LIMIT 1
-        ) as last_purchased_date FROM richealp7.product WHERE is_deleted = 0 ";
-if (isset($search) && $search) { $sql = $sql."AND name LIKE '%".$search."%' "; }
-$sql = $sql."HAVING last_purchased_date < DATE_SUB(NOW(), INTERVAL 1 MONTH) OR last_purchased_date IS NULL ORDER BY last_purchased_date ASC, creation_datetime ASC LIMIT ".$_ITEMS_PER_PAGE." OFFSET ".$offset;
+$sql = "SELECT category, CONCAT('[', SUBSTRING_INDEX(GROUP_CONCAT(CONCAT('[', product_id, ', ', seller_id, ']') ORDER BY times_wished_for DESC), ',', 6), ']') AS most_wished_for_product_ids FROM product_view WHERE times_wished_for != 0 GROUP BY category LIMIT $_ITEMS_PER_PAGE OFFSET $offset";
 $result = mysqli_query($conn, $sql);
-$products = array();
+$categories = array();
 
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
-        array_push($products, $row);
+        array_push($categories, $row);
     }
 }
 
-$vars = array('products' => $products, 'page' => $page, 'page_size' => $_ITEMS_PER_PAGE);
-if (isset($search) && $search) { $vars['search'] = $search; }
+foreach ($categories as &$category) {
+    $category['most_wished_for_product_ids'] = json_decode($category['most_wished_for_product_ids']);
+    $category['products'] = array();
+    foreach ($category['most_wished_for_product_ids'] as $product_info) {
+        $sql = "SELECT * FROM product_view WHERE product_id = ".$product_info[0]." AND seller_id = ".$product_info[1];
+        $result = mysqli_query($conn, $sql);
+        $product = mysqli_fetch_assoc($result);
+        $product['category_color'] = $_CATEGORY_COLORS[(int)substr((string)crc32($product['category']), 0, 1) % count($_CATEGORY_COLORS)];
+        array_push($category['products'], $product);
+    }
+}
 
-echo $twig->render('products_not_selling_well.html', $vars);
+echo $twig->render('products_most_wished.html', $vars = array('categories' => $categories, 'page' => $page, 'page_size' => $_ITEMS_PER_PAGE));
 
 ?>
